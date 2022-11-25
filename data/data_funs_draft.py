7c6a97d761419b266cbd2aa26d987bf5f0d3c368
Margherita-IDEMS <91319540+MargheritaPhilipp@users.noreply.github.com>
2022-11-25 17:42:26 +0100

    Merge branch 'main' of https://github.com/danidlsa/comp4ds_hw6
    not sure what went wrong

--- data/data_funs_draft.py
+++ data/data_funs_draft.py
@@ -8,7 +8,8 @@
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
 import numpy as np
@@ -62,7 +63,14 @@ df.columns[:-1]
 # b) Create a preprocessor class that removes those rows that contain NaN values in the columns:
 # age, gender, ethnicity.
         
+<<<<<<< Updated upstream
 class clean_demographics():
+=======
+df_test_clean1 = clean_demographics(diabetes_test)        
+df_test_clean1.df
+df_test_clean1= df_test_clean1.drop_selected_nans()        
+    
+>>>>>>> Stashed changes
 
 
 # c) Create a preprocessor class that fills NaN with the mean value of the column in the columns:
@@ -72,6 +80,10 @@ class clean_measurements():
 
     
 
+
+df_test_clean2 = clean_measurements(df_test_clean1).fill_nans()
+
+
 # d) Create at least two feature classes that transform some of the columns in the data set.
 # These feature classes need to have the same structure defined by an abstract parent class
 # (Remember: polymorphism).
@@ -80,6 +92,10 @@ class clean_measurements():
     # suggested features: one-hot encoding for dummies on a couple of categorical ones
     # e.g. hospital_admit_source  OR 	icu_admit_source
 
+df_test_eth= ethnicity_oh(df_test_clean2).one_hot_enc()
+
+df_test_gender= gender_oh(df_train_eth).one_hot_enc()
+
 
 # e) Create a model class with two primary methods: train and predict.
 # When the model class is initialized, the constructor (init) should receive as inputs (at least):
@@ -87,7 +103,43 @@ class clean_measurements():
 # 2. Target column that is going to be used
 # 3. (bonus) Hyperparameters of the model to be used.
 
+class Train_and_predict_RF:
+    def __init__(self, features_list:list, 
+                 target:str, 
+                 df_train,
+                 df_test,
+                 n_estimators:int, # hyperparameter tunning 1
+                 max_depth:int): # hyperparameter tunning 2
+        self.X_train=df_train.loc[:,features_list]
+        self.y_train=df_train.loc[:,[target]]
+        self.target=target
+        self.X_test=df_test.loc[:,features_list]
+        self.y_test=df_test.loc[:,[target]]
+        self.n_estimators=n_estimators
+        self.max_depth=max_depth
+        
+    def train(self):
+        self.model_tree= RandomForestClassifier(random_state=0,
+                                                max_depth=self.max_depth,
+                                                n_estimators=self.n_estimators)
+        self.model_tree.fit(self.X_train, self.y_train)
+        return self.model_tree
+    
+    def predict(self):
+        self.y_pred = self.model_tree.predict(self.X_test)
+        self.acc = accuracy_score(self.y_test, self.y_pred)
+        return print("Accuracy of the Random Forest model: " + str(self.acc))
+        
 
+# test
+
+features = ['age','height','weight','aids','cirrhosis', 'hepatic_failure', 'immunosuppression', 'leukemia', 'lymphoma', 'solid_tumor_with_metastasis']
+
+model_randomf = Train_and_predict_RF(features, "diabetes_mellitus", df_train_gender, df_test_gender, 8, 4)
+
+model_randomf.train()
+
+model_randomf.predict()
 
 # f) The model class should have as private attributes each of the inputs of the constructor 
 # and an additional one, called “model” that will be a model from sklearn chosen by the team
