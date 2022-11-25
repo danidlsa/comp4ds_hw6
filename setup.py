92055898a3d038c7f2e279cace4f0ef43545e21b
Margherita-IDEMS <91319540+MargheritaPhilipp@users.noreply.github.com>
2022-11-25 16:54:43 +0100

    :ok_woman: setup files created
    changed name of library in both and altered the link in setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+#######
+# Only needed if the version is imported from the library
+# import os
+# import sys
+
+# base_dir = os.path.dirname(__file__)
+# src_dir = os.path.join(base_dir, 'src')
+# sys.path.insert(0, src_dir)
+
+# import library_example
+#######
+
+def get_requirements(requirements_path='requirements.txt'):
+    with open(requirements_path) as fp:
+        return [x.strip() for x in fp.read().split('\n') if not x.startswith('#')]
+
+# for the version you can either put here a string with the version number
+__version__ = "0.1.0"
+# or you can create a variable called __version__ inside the __init__.py from
+# the src/your_library folder that has a string with the version as a string.
+
+setup(
+    name= 'hw6_library',
+    version='0.1.0',
+    description='Setting up a python package',
+    author='Daniela de los Santos & Margherita Philipp',
+    url='https://github.com/danidlsa/comp4ds_hw6.git',
+    packages=find_packages(where='src', exclude=['tests']),
+    package_dir={'': 'src'},
+    install_requires=get_requirements(),
+    setup_requires=['pytest-runner', 'wheel'],
+    package_data={'sample_diabetes_mellitus_data': ['sample_diabetes_mellitus_data.csv']
+                  }
+)
